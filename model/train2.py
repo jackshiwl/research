20cde899aa653c6d212843f960387406ad5c7731
Jack <jack.shi.wei.lun7@gmail.com>
2022-10-21 17:11:59 +0800

    update ens notebook

--- model/train2.py
+++ model/train2.py
@@ -28,97 +28,97 @@ def my_MSE_weighted(y_true, y_pred):
   weights= tf.clip_by_value(y_true, 0.01, 1)
   return K.mean(tf.multiply(weights, tf.abs(tf.subtract(y_pred, y_true))))
 
-def make_FSS_loss(mask_size):  # choose any mask size for calculating densities
+# def make_FSS_loss(mask_size):  # choose any mask size for calculating densities
 
-    def my_FSS_loss(y_true, y_pred):
+#     def my_FSS_loss(y_true, y_pred):
 
-        # First: DISCRETIZE y_true and y_pred to have only binary values 0/1 
-        # (or close to those for soft discretization)
-        want_hard_discretization = False
+#         # First: DISCRETIZE y_true and y_pred to have only binary values 0/1 
+#         # (or close to those for soft discretization)
+#         want_hard_discretization = False
 
-        # This example assumes that y_true, y_pred have the shape (None, N, N, 1).
+#         # This example assumes that y_true, y_pred have the shape (None, N, N, 1).
         
-        cutoff = 0.5  # choose the cut off value for discretization
+#         cutoff = 0.5  # choose the cut off value for discretization
 
-        if (want_hard_discretization):
-           # Hard discretization:
-           # can use that in metric, but not in loss
-           y_true_binary = tf.where(y_true>cutoff, 1.0, 0.0)
-           y_pred_binary = tf.where(y_pred>cutoff, 1.0, 0.0)
+#         if (want_hard_discretization):
+#            # Hard discretization:
+#            # can use that in metric, but not in loss
+#            y_true_binary = tf.where(y_true>cutoff, 1.0, 0.0)
+#            y_pred_binary = tf.where(y_pred>cutoff, 1.0, 0.0)
 
-        else:
-           # Soft discretization
-           c = 10 # make sigmoid function steep
-           y_true_binary = tf.math.sigmoid( c * ( y_true - cutoff ))
-           y_pred_binary = tf.math.sigmoid( c * ( y_pred - cutoff ))
+#         else:
+#            # Soft discretization
+#            c = 10 # make sigmoid function steep
+#            y_true_binary = tf.math.sigmoid( c * ( y_true - cutoff ))
+#            y_pred_binary = tf.math.sigmoid( c * ( y_pred - cutoff ))
 
-        # Done with discretization.
+#         # Done with discretization.
 
-        # To calculate densities: apply average pooling to y_true.
-        # Result is O(mask_size)(i,j) in Eq. (2) of [RL08].
-        # Since we use AveragePooling, this automatically includes the factor 1/n^2 in Eq. (2).
-        pool1 = tf.keras.layers.AveragePooling2D(pool_size=(mask_size, mask_size), strides=(1, 1), 
-           padding='same')
-        y_true_density = pool1(y_true_binary);
-        # Need to know for normalization later how many pixels there are after pooling
-        n_density_pixels = tf.cast( (tf.shape(y_true_density)[1] * tf.shape(y_true_density)[2]) , 
-           tf.float32 )
+#         # To calculate densities: apply average pooling to y_true.
+#         # Result is O(mask_size)(i,j) in Eq. (2) of [RL08].
+#         # Since we use AveragePooling, this automatically includes the factor 1/n^2 in Eq. (2).
+#         pool1 = tf.keras.layers.AveragePooling2D(pool_size=(mask_size, mask_size), strides=(1, 1), 
+#            padding='same')
+#         y_true_density = pool1(y_true_binary);
+#         # Need to know for normalization later how many pixels there are after pooling
+#         n_density_pixels = tf.cast( (tf.shape(y_true_density)[1] * tf.shape(y_true_density)[2]) , 
+#            tf.float32 )
 
-        # To calculate densities: apply average pooling to y_pred.
-        # Result is M(mask_size)(i,j) in Eq. (3) of [RL08].
-        # Since we use AveragePooling, this automatically includes the factor 1/n^2 in Eq. (3).
-        pool2 = tf.keras.layers.AveragePooling2D(pool_size=(mask_size, mask_size),
-                                                 strides=(1, 1), padding='same')
-        y_pred_density = pool2(y_pred_binary);
+#         # To calculate densities: apply average pooling to y_pred.
+#         # Result is M(mask_size)(i,j) in Eq. (3) of [RL08].
+#         # Since we use AveragePooling, this automatically includes the factor 1/n^2 in Eq. (3).
+#         pool2 = tf.keras.layers.AveragePooling2D(pool_size=(mask_size, mask_size),
+#                                                  strides=(1, 1), padding='same')
+#         y_pred_density = pool2(y_pred_binary);
 
-        # This calculates MSE(n) in Eq. (5) of [RL08].
-        # Since we use MSE function, this automatically includes the factor 1/(Nx*Ny) in Eq. (5).
-        MSE_n = tf.keras.losses.MeanSquaredError()(y_true_density, y_pred_density)
+#         # This calculates MSE(n) in Eq. (5) of [RL08].
+#         # Since we use MSE function, this automatically includes the factor 1/(Nx*Ny) in Eq. (5).
+#         MSE_n = tf.keras.losses.MeanSquaredError()(y_true_density, y_pred_density)
 
-        # To calculate MSE_n_ref in Eq. (7) of [RL08] efficiently:
-        # multiply each image with itself to get square terms, then sum up those terms.
+#         # To calculate MSE_n_ref in Eq. (7) of [RL08] efficiently:
+#         # multiply each image with itself to get square terms, then sum up those terms.
 
-        # Part 1 - calculate sum( O(n)i,j^2
-        # Take y_true_densities as image and multiply image by itself.
-        O_n_squared_image = tf.keras.layers.Multiply()([y_true_density, y_true_density])
-        # Flatten result, to make it easier to sum over it.
-        O_n_squared_vector = tf.keras.layers.Flatten()(O_n_squared_image)
-        # Calculate sum over all terms.
-        O_n_squared_sum = tf.reduce_sum(O_n_squared_vector)
+#         # Part 1 - calculate sum( O(n)i,j^2
+#         # Take y_true_densities as image and multiply image by itself.
+#         O_n_squared_image = tf.keras.layers.Multiply()([y_true_density, y_true_density])
+#         # Flatten result, to make it easier to sum over it.
+#         O_n_squared_vector = tf.keras.layers.Flatten()(O_n_squared_image)
+#         # Calculate sum over all terms.
+#         O_n_squared_sum = tf.reduce_sum(O_n_squared_vector)
 
-        # Same for y_pred densitites:
-        # Multiply image by itself
-        M_n_squared_image = tf.keras.layers.Multiply()([y_pred_density, y_pred_density])
-        # Flatten result, to make it easier to sum over it.
-        M_n_squared_vector = tf.keras.layers.Flatten()(M_n_squared_image)
-        # Calculate sum over all terms.
-        M_n_squared_sum = tf.reduce_sum(M_n_squared_vector)
+#         # Same for y_pred densitites:
+#         # Multiply image by itself
+#         M_n_squared_image = tf.keras.layers.Multiply()([y_pred_density, y_pred_density])
+#         # Flatten result, to make it easier to sum over it.
+#         M_n_squared_vector = tf.keras.layers.Flatten()(M_n_squared_image)
+#         # Calculate sum over all terms.
+#         M_n_squared_sum = tf.reduce_sum(M_n_squared_vector)
     
-        MSE_n_ref = (O_n_squared_sum + M_n_squared_sum) / n_density_pixels
+#         MSE_n_ref = (O_n_squared_sum + M_n_squared_sum) / n_density_pixels
         
-        # FSS score according to Eq. (6) of [RL08].
-        # FSS = 1 - (MSE_n / MSE_n_ref)
+#         # FSS score according to Eq. (6) of [RL08].
+#         # FSS = 1 - (MSE_n / MSE_n_ref)
 
-        # FSS is a number between 0 and 1, with maximum of 1 (optimal value).
-        # In loss functions: We want to MAXIMIZE FSS (best value is 1), 
-        # so return only the last term to minimize.
+#         # FSS is a number between 0 and 1, with maximum of 1 (optimal value).
+#         # In loss functions: We want to MAXIMIZE FSS (best value is 1), 
+#         # so return only the last term to minimize.
 
-        # Avoid division by zero if MSE_n_ref == 0
-        # MSE_n_ref = 0 only if both input images contain only zeros.
-        # In that case both images match exactly, i.e. we should return 0.
-        my_epsilon = tf.keras.backend.epsilon()  # this is 10^(-7)
+#         # Avoid division by zero if MSE_n_ref == 0
+#         # MSE_n_ref = 0 only if both input images contain only zeros.
+#         # In that case both images match exactly, i.e. we should return 0.
+#         my_epsilon = tf.keras.backend.epsilon()  # this is 10^(-7)
 
-        if (want_hard_discretization):
-           if MSE_n_ref == 0:
-              return( MSE_n )
-           else:
-              return( MSE_n / MSE_n_ref )
-        else:
-           return (MSE_n / (MSE_n_ref + my_epsilon) )
+#         if (want_hard_discretization):
+#            if MSE_n_ref == 0:
+#               return( MSE_n )
+#            else:
+#               return( MSE_n / MSE_n_ref )
+#         else:
+#            return (MSE_n / (MSE_n_ref + my_epsilon) )
 
-    return my_FSS_loss 
+#     return my_FSS_loss 
 
-mask_size = 4 
+# mask_size = 4 
 n_classes = 4
 
 def weighted_categorical_crossentropy(weights):
@@ -141,13 +141,15 @@ downscale_factor = 12
 # load low resolution other variables data for training
 # load low resolution data for training reforecast
 PATH = '/scratch/users/nus/e0560091/data/'
-reforecast_train=load(PATH+'X_train_apcp.npy')
+# reforecast_train=load(PATH+'X_train_apcp.npy') # DETERMINISTIC
+reforecast_train=load(PATH+'X_train_ensemble.npy')   # ENSEMBLE
 
 # load high resolution data for training WRF
 wrf_train=load(PATH+'wrf_train.npy')
 
 #load low resolution data for validation
-reforecast_val=load(PATH+'X_val_apcp.npy')
+# reforecast_val=load(PATH+'X_val_apcp.npy') # DETERMINISTIC
+reforecast_val=load(PATH+'X_val_ensemble.npy')   # ENSEMBLE
 
 #load high resolution data for validation
 wrf_val=load(PATH+'wrf_val.npy')
@@ -219,6 +221,6 @@ def train(epochs, batch_size):
         
 
 
-train(100, 64)
+train(150, 64) # 100,64
 
 
